f585e7c9e5f9cfab194f39e0d914822918cbe056
Dana Ottaviani <dana.ottaviani@gmail.com>
2016-07-26 16:57:20 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+/*Referenced at https://css-tricks.com/snippets/jquery/smooth-scrolling/ and David Moran*/
+
+$('body').scrollspy({ target: '#navbar-example' });
+
+$('[data-spy="scroll"]').each(function () {
+  var $spy = $(this).scrollspy('refresh')
+});
+
+$(document).ready(function(){
+  $('a').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+    && location.hostname == this.hostname) {
+      var $target = $(this.hash);
+      $target = $target.length && $target
+      || $('[name=' + this.hash.slice(1) +']');
+      if ($target.length) {
+        var targetOffset = $target.offset().top - 170;
+        $('html,body')
+        .animate({scrollTop: targetOffset}, 1000);
+       return false;
+      }
+    }
+  });
+});
+
